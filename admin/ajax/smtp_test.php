3e446367371e149aa7c73ccc98083f79319bebeb
Дмитрий Дементьев <info@dementiev.net>
2025-07-09 10:35:51 +0300

    добавляет логирование в ajax

--- admin/ajax/smtp_test.php
+++ admin/ajax/smtp_test.php
@@ -6,9 +6,13 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.
 
 use Bitrix\Main\Localization\Loc;
 use DDAPP\Tools\CustomMail;
+use DDAPP\Tools\Helpers\LogHelper;
 use DDAPP\Tools\Helpers\UserHelper;
 
 Loc::loadMessages(__FILE__);
+
+// Настройка логирования
+LogHelper::configure();
 
 // Проверка сессии Bitrix
 if (!check_bitrix_sessid()) {
@@ -25,7 +29,15 @@ if (UserHelper::hasModuleAccess("") != "W") {
 try {
     $mailer = new CustomMail();
     $result = $mailer->testConnection();
+
+    if ($result["success"]) {
+        LogHelper::info("smtp", "SMTP Test", $result["debug"]);
+    } else {
+        LogHelper::error("smtp", "SMTP Test Error", $result["debug"]);
+    }
+
     echo json_encode($result);
 } catch (Exception $e) {
+    LogHelper::error("smtp", "SMTP Test Error", $e->getMessage());
     echo json_encode(["success" => false, "message" => $e->getMessage()]);
 }

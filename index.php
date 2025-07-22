e021177083e5f50cf77e301ab0b533661230ee21
Дмитрий Дементьев <info@dementiev.net>
2025-07-22 21:19:19 +0300

    тестовые страницы для вызова компонентов

--- index.php
+++ index.php
@@ -10,9 +10,16 @@ $APPLICATION->AddHeadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/j
 $APPLICATION->SetAdditionalCSS("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css");
 
 ?>
-
     <section class="container my-5">
-        TEST
+        <h1>ГЛАВНАЯ</h1>
+        <p>
+            <a href="/test/auth/"
+               class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Авторизация</a>
+        </p>
+        <p>
+            <a href="/test/forms/"
+               class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Формы</a>
+        </p>
     </section>
 
 <?php
